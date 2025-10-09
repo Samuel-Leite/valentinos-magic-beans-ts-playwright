eb9e4983807f2c6cc83be236eab276a4db404fd3
Samuel Leite <samuel-leite@outlook.com.br>
2025-10-09 17:55:55 -0300

    fix(all): 🐛 update variable build and project nam

--- src/integrations/browserstack/endpointBuilder.ts
+++ src/integrations/browserstack/endpointBuilder.ts
@@ -35,6 +35,8 @@ export class EndpointBuilder {
     const finalCaps = {
       ...caps,
       name: testName,
+      build: process.env.BUILD_NAME,
+      project: process.env.PROJECT_NAME,
       'browserstack.username': process.env.BROWSERSTACK_USERNAME,
       'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
     };
