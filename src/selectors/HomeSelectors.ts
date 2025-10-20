3f609d3e81701673061f9d7c86fc8096dcfb6a26
Samuel-Leite <Samuel-Leite@users.noreply.github.com>
2025-10-20 00:03:35 +0000

    deploy: 76ca2f9ab92332ec578f8027419181aa8fd128a8

--- src/selectors/HomeSelectors.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from '@playwright/test';
-
-export const getHomeLocators = (page: Page) => ({
-  toastLoginSuccess: page.locator('li[role="status"] .font-semibold'),
-  btnUserMenu: page.locator('button:has(svg.lucide-user)'),
-  btnLogOut: page.locator('text=Log out'),
-});
--- src/selectors/LoginSelectors.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@playwright/test';
-
-export const getLoginLocators = (page: Page) => ({
-  btnDoLogin: page.locator('[data-test-id="header-login-button-desktop"]'),
-  txtEmail: page.locator('[data-test-id="login-email-input"]'),
-  txtPassword: page.locator('[data-test-id="login-password-input"]'),
-  btnSubmit: page.locator('[data-test-id="login-submit-button"]'),
-});
